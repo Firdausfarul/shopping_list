9c92e3b5f55e051f47d89fca920ebe9af35ac8e5
Firdausfarul <Firdausfarul@gmail.com>
2023-09-27 15:50:03 +0700

    tutorial 4 27/09/2023

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from main.views import show_main, create_product
 app_name = 'main'
-from main.views import register, login_user, logout_user, show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import register, login_user, logout_user, show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, edit_product, delete_product
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
@@ -12,4 +12,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'), 
     path('logout/', logout_user, name='logout'),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'),
 ]
--- main/views.py
+++ main/views.py
@@ -12,7 +12,28 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import datetime
 
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
 
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk = id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
 
 def login_user(request):
     if request.method == 'POST':
